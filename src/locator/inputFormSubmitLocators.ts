efce634fa01f0c998eccb192069c751710261b3d
Shubham <skchoudhry5112000@gmail.com>
2025-09-14 19:18:49 +0530

    Added the src folder with the four subfolder

---
+++ src/locator/inputFormSubmitLocators.ts
@@ -0,0 +1,4 @@
+export class inputFormSubmit{
+    static readonly inputFormSubmitLink = '//a[text()="Input Form Submit"]'
+    static readonly submitButton = '//*[@id="seleniumform"]/div[6]/button'
+}
